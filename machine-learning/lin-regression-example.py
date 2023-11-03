5747ce0d2cd648d437b18464d3eed3148e43b8c9
micki <github@com-science.de>
2023-11-03 21:01:46 +0100

    graceful shutdown

--- machine-learning/lin-regression-example.py
+++ machine-learning/lin-regression-example.py
@@ -21,29 +21,33 @@ def animate(i):
     plt.plot(x, f(theta, x))
 
 def task():
-    global theta
-    global trainings_set
-    global y
+  global theta
+  global trainings_set
+  global y
 
-    for i in range(100):
-        delta = np.array([
-            h(theta, trainings_set[0]) - y[0],
-            h(theta, trainings_set[1]) - y[1],
-            h(theta, trainings_set[2]) - y[2],
-            ])
-        d_J_theta = np.array([
-            (1/3) * (delta[0] + delta[1] + delta[2]),
-            (1/3) * (delta[0]*trainings_set[0][1]
-                     + delta[1]*trainings_set[1][1] 
-                     + delta[2]*trainings_set[2][1]
-            )])
+  for i in range(100):
+      if ui_terminated:
+          break
 
-        theta = np.array([
-            theta[0] - alpha * d_J_theta[0], 
-            theta[1] - alpha * d_J_theta[1]
-            ])
-        print("theta:", theta)
-        sleep(0.5)
+      delta = np.array([
+          h(theta, trainings_set[0]) - y[0],
+          h(theta, trainings_set[1]) - y[1],
+          h(theta, trainings_set[2]) - y[2],
+          ])
+      d_J_theta = np.array([
+          (1/3) * (delta[0] + delta[1] + delta[2]),
+          (1/3) * (delta[0]*trainings_set[0][1]
+                   + delta[1]*trainings_set[1][1] 
+                   + delta[2]*trainings_set[2][1]
+          )])
+
+      theta = np.array([
+          theta[0] - alpha * d_J_theta[0], 
+          theta[1] - alpha * d_J_theta[1]
+          ])
+      print("theta:", theta)
+      sleep(0.5)
+  print("worker thread finished")
 
 alpha = 0.3
 trainings_set = np.array([
@@ -56,6 +60,7 @@ y = np.array([1,2,3])
 
 theta = np.array([0,0])
 
+ui_terminated = False
 thread = Thread(target=task)
 print("Now start the gradient descent task")
 thread.start()
@@ -63,5 +68,6 @@ thread.start()
 print("Plot the resulting linear line")
 ani = animation.FuncAnimation(plt.figure(), animate, interval=500)
 plt.show()
+ui_terminated = True
 
 thread.join()
