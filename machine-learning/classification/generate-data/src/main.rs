2bcdfe6e22d86fe3f12aa63c80b4cd9741b603b8
micki <github@com-science.de>
2023-12-03 22:03:39 +0100

    Add data generator in Rust

---
+++ machine-learning/classification/generate-data/src/main.rs
@@ -0,0 +1,21 @@
+use rand::Rng;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+
+    let mut counter = 0;
+    loop {
+        let rand_sample = rng.gen_range(0.0..1.0);
+        if 0.7 <= rand_sample && rand_sample <= 0.8 {
+            println!("{}, 1", rand_sample);
+
+            counter += 1;
+
+            if counter == 100 {
+                break;
+            }
+        } else {
+            println!("{}, 0", rand_sample);
+        }
+    }
+}
